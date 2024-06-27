f93377a68bf3dab9679721690d33311c445771cd
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-06-27 17:12:12 +0800

    優化啟動方式 python資料夾不存在 解壓縮python7zip

--- start_main.bat
+++ start_main.bat
@@ -1,4 +1,27 @@
 @echo off
 set current_dir=%~dp0
-"%current_dir%\Python-3.12.4\python.exe" "%current_dir%\main.py"
+set python_dir=%current_dir%
+set seven_zip_dir=%current_dir%7-Zip Extra
+
+:: Check if the Python-3.12.4 directory exists
+if not exist "%python_dir%python-3.12.4" (
+    echo Python-3.12.4 directory does not exist, extracting...
+    "%seven_zip_dir%\7za.exe" x "%current_dir%python-3.12.4.7z" -o"%python_dir%"
+    if errorlevel 1 (
+        echo Extraction failed.
+        pause
+        exit /b 1
+    )
+    echo Extraction completed.
+)
+
+:: Ensure paths are correctly formatted
+set "python_exe=%python_dir%\python-3.12.4\python.exe"
+
+:: Run the Python script
+if exist "%python_exe%" (
+    "%python_exe%" "%current_dir%main.py"
+) else (
+    echo Python executable not found at "%python_exe%".
+)
 pause
